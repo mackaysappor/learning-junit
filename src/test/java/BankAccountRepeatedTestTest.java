d44bc2591c09bb5a43dbc9007cadbe334666945b
mackaysappor <labs.mackay@gmail.com>
2023-01-05 23:27:59 +0000

    Repeated tests

---
+++ src/test/java/BankAccountRepeatedTestTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+// Repeating tests a number of times using annotations
+
+@ExtendWith(BankAccountParameterResolver.class)
+public class BankAccountRepeatedTestTest {
+    @RepeatedTest(5) // we use the @RepeatedTeest annotation to repeat tests based on the number specified
+    @DisplayName("Deposited 500 successfully")
+    public void testDeposit(BankAccount bankAccount) {
+        bankAccount.deposit(500);
+        assertEquals(500, bankAccount.getBalance());
+    }
+}
